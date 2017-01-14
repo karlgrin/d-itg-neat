f240fb3192b0138121723085050a1a8fcb40a95a
Carl Philip Matsson <caphmatsson@gmail.com>
2017-01-14 04:58:17 -0800

    add neat submodule

--- NEAT/examples/util.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXAMPLES_UTIL_INCLUDE_H
-#define EXAMPLES_UTIL_INCLUDE_H
-
-int read_file(const char *filename, char **bufptr);
-
-#endif /* ifndef EXAMPLES_UTIL_INCLUDE_H */
--- NEAT/neat_usrsctp_internal.h
+++
@@ -1,9 +0,0 @@
-#ifndef NEAT_USRSCTP_INTERNAL
-#define NEAT_USRSCTP_INTERNAL
-
-
-struct neat_ctx *neat_usrsctp_init_ctx(struct neat_ctx *nic);
-
-void neat_usrsctp_close_sockflow(struct neat_flow *fl);
-
-#endif
